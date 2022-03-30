bd251c3923b1df083de602a83272c9621c519cf0
akshitagarg275 <akshitagarg275@gmail.com>
2022-03-30 09:49:05 +0530

    day 9

--- django_codes/00hello/namaste/myapp/views.py
+++ django_codes/00hello/namaste/myapp/views.py
@@ -1,3 +1,8 @@
+import imp
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Hello")
--- django_codes/00hello/namaste/namaste/urls.py
+++ django_codes/00hello/namaste/namaste/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.http import HttpResponse
+from django.urls import path,include
+
+# def home(request):
+#     return HttpResponse("<h1>Hello</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('',home)
+    path('',include('myapp.urls')),
 ]
